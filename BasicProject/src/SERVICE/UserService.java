a4fd2e1349eb592eb6b00d68f49dc967740325f1
PC-14 <PC-14@DESKTOP-MQVS9UU.mshome.net>
2024-10-11 15:20:47 +0900

    다 수정함

--- BasicProject/src/SERVICE/UserService.java
+++ BasicProject/src/SERVICE/UserService.java
@@ -1,5 +1,31 @@
 package SERVICE;
 
-public class UserService {
+import kr.or.ddit.dao.UserDaoImpl;
+import kr.or.ddit.vo.UserVO;
 
+public class UserService{
+	private UserDaoImpl dao;
+	
+	// 싱글톤 ----------------------------
+	private static UserService service;
+	
+	private UserService() {
+		dao = UserDaoImpl.getInstance();
+	}
+	
+	public static UserService getInstance() {
+		if(service==null) {
+			service = new UserService();
+		}
+		return service;
+	}
+	//----------------------------------------------
+	
+	public int insertUser(UserVO userVo) {
+		return dao.insertUser(userVo);
+	}
+	
+	public UserVO getUser(UserVO userVo) {
+		return dao.getUser(userVo);
+	}
 }
---
+++ BasicProject/src/kr/or/ddit/util/Command.java
@@ -0,0 +1,22 @@
+package kr.or.ddit.util;
+
+public enum Command {
+	
+	HOME, 			// 첫화면
+	USER_HOME,		// 로그인 성공 후 화면
+	END,			// 종료
+	
+	// 회원 관련
+	LOGIN,			// 로그인
+	JOIN,   		// 회원가입
+	MYPAGE, 		// 내 정보 수정
+	
+	// 게시판 관련
+	BOARD_LIST,		// 게시판 리스트
+	BOARD_VIEW,		// 게시글 상세보기
+	BOARD_INSERT,	// 게시글 추가
+	BOARD_UPDATE,	// 게시글 수정
+	BOARD_DELETE	// 게시글 삭제
+	
+}
+
