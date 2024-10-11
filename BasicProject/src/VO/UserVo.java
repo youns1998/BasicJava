c88f39f83cea38e1614537c5fd296e496313e35a
PC-13 <PC-13@DESKTOP-29DR6HE>
2024-10-11 15:16:29 +0900

    유저 삽입

--- BasicProject/src/VO/UserVo.java
+++ BasicProject/src/VO/UserVo.java
@@ -1,5 +1,33 @@
 package VO;
 
 public class UserVo {
+	private String user_id;
+	private String user_pass;
+	private String user_name;
+	
+	public String getUser_id() {
+		return user_id;
+	}
+	public void setUser_id(String user_id) {
+		this.user_id = user_id;
+	}
+	public String getUser_pass() {
+		return user_pass;
+	}
+	public void setUser_pass(String user_pass) {
+		this.user_pass = user_pass;
+	}
+	public String getUser_name() {
+		return user_name;
+	}
+	public void setUser_name(String user_name) {
+		this.user_name = user_name;
+	}
+	
+	@Override
+	public String toString() {
+		return "UserVO [user_id=" + user_id + ", user_pass=" + user_pass + ", user_name=" + user_name + "]";
+	}
+	
 
 }
